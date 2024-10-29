fd3c41c742eaac14d65485439ffaeaf3d4509ddb
ponchik982 <aklimenkov34@gmail.com>
2024-10-29 09:58:39 +0300

    Я сделал LR2

---
+++ LR2/LR2/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+        public enum Genre
+        {
+            Action,
+            RPG,
+            Sports
+        }
+    
+}
